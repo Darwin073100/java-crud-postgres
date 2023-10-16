7a5beed7491cbb0d4bc1858147078a49944ed139
Edwin Garcia Quiterio <edwinegq@outlook.com>
2023-10-15 19:09:00 -0600

    add configuration with DB

--- src/main/java/com/connectiondb/Main.java
+++ src/main/java/com/connectiondb/Main.java
@@ -1,7 +1,9 @@
 package com.connectiondb;
 
+import com.connectiondb.util.DataBaseConnection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DataBaseConnection.getConnection();
     }
 }
---
+++ src/main/java/com/connectiondb/util/DataBaseConnection.java
@@ -0,0 +1,22 @@
+package com.connectiondb.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBaseConnection {
+    private final static String URL = "jdbc:postgresql://localhost:5432/project";
+    private final static String USER = "postgres";
+    private final static String PASSWORD = "admin123";
+    private static Connection conn;
+
+    public static Connection getConnection(){
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection successfully");
+            return conn;
+        } catch (Exception e) {
+            System.err.println(e.getMessage() + " :: " + e.getClass().getName());
+        }
+        return null;
+    }
+}
